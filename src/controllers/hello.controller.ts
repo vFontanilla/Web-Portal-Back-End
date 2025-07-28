baf472310bacee1c8849a7f7d907a09f1a20bc72
vFontanilla <vonfontanilla22@gmail.com>
2025-07-29 02:28:33 +0800

    asasas

--- src/controllers/hello.controller.ts
+++
@@ -1,5 +0,0 @@
-import { Request, Response } from 'express';
-
-export const getHello = (req: Request, res: Response) => {
-  res.json({ message: 'Hello from Express + TypeScript!' });
-};
--- src/routes/hello.route.ts
+++
@@ -1,8 +0,0 @@
-import { Router } from 'express';
-import { getHello } from '../controllers/hello.controller';
-
-const router = Router();
-
-router.get('/', getHello);
-
-export default router;
